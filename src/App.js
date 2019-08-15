d9f251ffd2e04da363f0934c0abae58d9d81a388
Colin Lyons <Colinlyons53@gmail.com>
2019-08-14 22:00:56 -0700

    created body and header components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Body from './components/Body'
+// import Footer from './components/Footer'
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
+      <Body />
+     {/* <Footer /> */}
 
      
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+    return(
+        <h1>Select a Meme, but dont pick the same meme more then once. </h1>
+    )
+}
+
+export default Header
