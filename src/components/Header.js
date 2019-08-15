203ddcd5186054981e4216d1aec92f672972ef6e
Colin Lyons <Colinlyons53@gmail.com>
2019-08-15 10:55:23 -0700

    updated header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Header() {
     return(
+        <div className="header">
         <h1>Select a Meme, but dont pick the same meme more then once. </h1>
+    <div className="title">{props.children}</div>
+    <div className="scores">
+      Score: {props.score} Highscore: {props.highscore}
+    </div>
+  </div>
     )
 }
 
