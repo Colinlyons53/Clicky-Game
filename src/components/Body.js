8197ae5c6e4641d3a02604cf59b8f9af6ec6584b
Colin Lyons <Colinlyons53@gmail.com>
2019-08-16 16:18:12 -0700

    added Body.css

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,6 @@
-import React from 'react'
+import React from "react";
+import "./Body.css";
 
-function Body() {
-    return (<div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-4">Fluid jumbotron</h1>
-      <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-    </div>
-  </div>)
-}
-export default Body
+const Body = props => <div className="Body">{props.children}</div>;
+
+export default Body;
